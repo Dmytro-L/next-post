d442a46a37012bc24e1ea4c7d18dd887268a91c0
Dima Lishnovskiy <dlishnovkiy@intelliboard.net>
2023-12-04 13:35:10 +0200

    add seo, build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "955efe24c6fe22af"; }
+/******/ 		__webpack_require__.h = function() { return "eb195881a79346e1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,20 @@ import Home from "@/components/screens/home/Home";
 import { IPostData } from "@/interfaces/post.interface";
 import { PostService } from "@/services/post.service";
 import { GetStaticProps, NextPage } from "next";
+import Head from 'next/head';
+
 
 const HomePage: NextPage<IPostData> = ({ posts }) => {
-  return <Home posts={posts} />;
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+        <meta name="description" content="description" />
+        <meta name="keywords" content="keywords" />
+      </Head>
+      <Home posts={posts} />
+    </>
+  )
 };
 
 export const getStaticProps: GetStaticProps<IPostData> = async () => {
